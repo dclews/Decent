fa4aff28c9df2e751584ca94beceb3800d0b7de1
snwmelt <ekitai.k@gmail.com>
2014-07-05 11:27:02 +0100

    Initial Push

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Decent.dotNET.ui;
+using Decent.dotNET.ui.windowFrame;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Decent.dotNET.ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : modernWindowFrame
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            setWindowAppearance();
+        }
+
+        private void centerWindowOnDisplay()
+        {
+            Left = ((FormsScreenInterface.currentDisplayAvailableBounds()[2] / 2) - (Width / 2));
+            Top  = ((FormsScreenInterface.currentDisplayAvailableBounds()[3] / 2) - (Height / 2));
+        }
+
+        private void setWindowAppearance()
+        {
+            BorderThickness = new Thickness(1);
+            BorderBrush     = new SolidColorBrush(Colors.Black);
+
+            // ^ should be set via theme manager
+
+            ResizeHandle = resizeHandle.ResizeGrip;
+            MinHeight    = 530;
+            MinWidth     = 730;
+
+            setWindowDimentions();
+            centerWindowOnDisplay();
+        }
+
+        private void setWindowDimentions()
+        {
+            // needs to be changed to fetch dimentions from prior run
+            Height = 530;
+            Width  = 730;
+        }
+    }
+}
---
+++ src/dotNET/ui/userControlInput.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Decent.dotNET.ui
+{
+    /// <summary>
+    /// Interaction logic for userControlInput.xaml
+    /// </summary>
+    public partial class userControlInput : UserControl
+    {
+        public userControlInput()
+        {
+            InitializeComponent();
+        }
+
+        public void sendContent()
+        {
+
+        }
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+
+            Console.WriteLine(UIElementTextBox.SelectedText);
+        }
+    }
+}
